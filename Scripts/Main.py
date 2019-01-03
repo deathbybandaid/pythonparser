4a8d14c498fa147e032c790a0f102aeb1beac55c
deathbybandaid <sam@deathbybandaid.net>
2019-01-03 16:55:54 -0500

    progress

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -5,6 +5,10 @@
 import os
 import sys
 
+# other imports
+import time
+import socket
+
 
 """
 This is the central script
@@ -16,16 +20,86 @@ def mainfunction():
     # create dynamic class
     dbbparse = class_create('dbbparse')
 
-    print 'herea'
-
     # save relative paths of interest
     dbbparse = relativepaths(dbbparse)
 
-    print 'hereb'
+    # Logo
+    dbb_avatar(dbbparse)
+    print('\n' * 2)
 
-    print str(dbbparse.paths)
+    osd(textarray='Cleaning Temp Directory.', color='YELLOW')
+    tempclean(dbbparse)
+    print('\n' * 2)
 
-    # dbb_avatar(dbbparse)
+    # Check internet connection
+    osd(textarray='Checking for internet connection.', color='YELLOW')
+    if not internet():
+        osd(textarray='Internet appears to be down!', color='RED')
+        return
+    osd(textarray='Internet connection success!!', indent=1, color='GREEN')
+    print('\n' * 2)
+
+
+"""
+Network Functions
+"""
+
+
+def internet(host="8.8.8.8", port=53, timeout=3):
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except Exception as ex:
+        return False
+
+
+"""
+Display Functions
+"""
+
+
+def dbb_avatar(dbbparse):
+    filepath = os.path.join(dbbparse.paths['scripts_common'], 'avatar.txt')
+    lines = [line.rstrip('\n') for line in open(filepath)]
+    osd(textarray=lines)
+
+
+def osd(textarray=[], indent=0, color='BOLD'):
+
+    if not isinstance(textarray, list):
+        textarray = [str(textarray)]
+
+    if len(textarray) == 0:
+        return
+
+    try:
+        coloreval = eval('bcolors.' + color.upper())
+        endcolor = bcolors.ENDC
+    except AttributeError:
+        coloreval = ''
+        endcolor = ''
+
+    for entry in textarray:
+        indentstr = ''
+        if indent:
+            if not isinstance(indent, int):
+                indent = 1
+            indentstr = "     " * indent
+        print indentstr + coloreval + entry + endcolor
+
+
+"""
+File Structures
+"""
+
+
+def rm(f):
+    if os.path.isdir(f):
+        return os.rmdir(f)
+    if os.path.isfile(f):
+        return os.unlink(f)
+    return
 
 
 def relativepaths(dbbparse):
@@ -33,18 +107,33 @@ def relativepaths(dbbparse):
 
     dbbparse.paths['scripts'] = os.path.split(os.path.abspath(os.path.realpath(sys.argv[0])))[0]
 
+    dbbparse.paths['scripts_common'] = os.path.join(dbbparse.paths['scripts'], 'Common')
+
     dbbparse.paths['root'] = os.path.dirname(dbbparse.paths['scripts'])
+
+    dbbparse.paths['temp'] = os.path.join(dbbparse.paths['root'], 'Temp')
 
     return dbbparse
 
 
-def dbb_avatar():
-    filepath = 'sadsdf'
-    inf = codecs.open(filepath, "r", encoding='utf-8')
-    infread = inf.read()
-    inf.close()
-    for line in infread:
-        print line
+def tempclean(dbbparse):
+    for root, dirs, files in os.walk(dbbparse.paths['temp'], topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    placeholderfile = os.path.join(dbbparse.paths['temp'], 'PERMANENT_PLACEHOLDER')
+    open(placeholderfile, 'a').close()
+    for tempsub in ['Bak', 'Processing']:
+        tempsubdir = os.path.join(dbbparse.paths['temp'], tempsub)
+        os.mkdir(tempsubdir)
+        placeholderfile = os.path.join(tempsubdir, 'PERMANENT_PLACEHOLDER')
+        open(placeholderfile, 'a').close()
+
+
+"""
+Classes
+"""
 
 
 def class_create(classname):
@@ -62,6 +151,16 @@ def class_create(classname):
     exec(compile("class class_" + str(classname) + ": " + compiletext, "", "exec"))
     newclass = eval('class_'+classname+"()")
     return newclass
+
+
+class bcolors:
+    PURPLE = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
 
 
 """
